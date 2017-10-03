0c27c6e3adda7c0a4de2b7cbd80770a186d4a4b0
spongefin <tikkha1@gmail.com>
2017-10-03 15:09:17 +0300

    Add files via upload

--- sptfy/main.js
+++ sptfy/main.js
@@ -43,24 +43,40 @@ $(document).ready(function () {
 			if (tracksAvailable[i].preview_url) {
 				counter = counter + 1;
 				trackList.push(tracksAvailable[i].preview_url);
-				$( "#demo" ).append("<ul id='playlistObject' style='max-width:90%;height:3.3em;' onclick='setPlayer(\"" +
+				$( "#demo" ).append("<div style='display:inline-block;width:30em;'><ul id='playlistObject' class='playlistObject' style='max-width:90%;height:3.3em;' onclick='setPlayer(\"" +
 				tracksAvailable[i].preview_url + "\",\"" +
 				tracksAvailable[i].album.name + "\",\"" +
 				tracksAvailable[i].artists[0].name + "\",\"" +
-				tracksAvailable[i].album.images[0].url +"\");'><div> <img style='display:inline-block;margin-left:-2.45em;margin-right:1em;margin-bottom:1.5em;'alt='Album picture' height='50' width='50' src="+
-				tracksAvailable[i].album.images[0].url+"><div style='display:inline-block;'><p id='songName' style='margin-bottom:0'>"+
-				tracksAvailable[i].name+ " "+"</p>"+"<p id='artistName' style='margin-bottom:0;color:darkcyan'>"+
+				tracksAvailable[i].album.images[0].url +"\");'><div><img class='aimage' style='display:inline-block;margin-left:-2.45em;margin-right:1em;margin-bottom:1.5em;'alt='Album picture' height='50' width='50' src="+
+				tracksAvailable[i].album.images[0].url+"><div style='display:inline-block;width:50%;'><p class='songName' id='songName' style='margin-bottom:0;text-overflow:ellipsis;white-space:nowrap;'>"+
+				tracksAvailable[i].name+ " "+"</p>"+"<p id='artistName' style='margin-bottom:0;color:darkcyan;text-overflow:ellipsis;white-space:pre;'>"+
 				tracksAvailable[i].artists[0].name+
 				"</p>"+"</div>"+
-				"</div> </ul>"
+				"</div>"+"</ul></div><p style='display:inline-block'>+</p>"
 				);	
 			}
 		}
-		//console.log(trackList);
-	}
+		
+	}//ongettracks
+	
+	var pressTimer;
+	
+	$("demo").mouseup(function(){
+	clearTimeout(pressTimer);
+	// Clear timeout
+	return false;
+	}).mousedown(function(){
+	// Set timeout
+	pressTimer = window.setTimeout(function() { 
+		console.log('test');
+	},1000);
+	return false;
+	});
 	
 });
 
-//+"<span><i class='fa fa-play' aria-hidden='true' style='margin-left:2em;'></i></span>"
+//+"<span><i class='fa fa-plus' aria-hidden='true' style='margin-left:2em;'></i></span>"
 
 //style='border: 1px solid cyan;border-radius:5px;
+
+//"<button class='btn' style='background-color:cyan;'>Add to playlist</button>"+
